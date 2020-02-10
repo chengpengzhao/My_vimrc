1b0c78bf0d23b3890402b50b129d9fa35a311cf9
chengpengzhao <chengpengzhao@foxmail.com>
2020-02-10 10:59:53 +0900

    修改初始化sh

--- initial_ssh.sh
+++ initial_ssh.sh
@@ -1,57 +1,60 @@
 #!/bin/bash
 
-sudo -i # 先运行这个进入root
-
-cd /etc/apt/
-if [  -f /etc/apt/sources.list ]; then
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.old
-fi
-#这个sources.list文件就是源文件，删除该文件，重新写一个
-sudo rm /etc/apt/sources.list
-sudo echo "deb-src http://archive.ubuntu.com/ubuntu xenial main restricted #Added by software-properties
-deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted multiverse universe #Added by software-properties
-deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted multiverse universe #Added by software-properties
-deb http://mirrors.aliyun.com/ubuntu/ xenial universe
-deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
-deb http://mirrors.aliyun.com/ubuntu/ xenial multiverse
-deb http://mirrors.aliyun.com/ubuntu/ xenial-updates multiverse
-deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse #Added by software-properties
-deb http://archive.canonical.com/ubuntu xenial partner
-deb-src http://archive.canonical.com/ubuntu xenial partner
-deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted
-deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted multiverse universe #Added by software-properties
-deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe
-deb http://mirrors.aliyun.com/ubuntu/ xenial-security multiverse" > sources.list
-
+#用法： yes Yes | ./initial.sh
+# 最开始把软件源先配置好
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install git
 sudo apt-get install curl -y
 sudo apt-get install vim-gtk3
 sudo apt-get install zsh
-chsh -s $(which zsh) #设置zsh为默认终端
+sudo apt install build-essential cmake python3-dev
+sudo apt-get install python3-pip
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #安装oh-my-zsh
+#安装后会提示进入zsh，后面的命令会被忽略
+echo "============================ \n"
+echo "打开initial.sh查看后续步骤！"
+exit
+# ==========================================================================
+# 第二步，配置zsh和vim
+cd
+
 sudo apt-get install autojump;git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions;git clone git://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting  #安装autojump、zsh-autosuggestions、zsh-syntax-highlighting三个插件
 
-cd
+chsh -s $(which zsh) #设置zsh为默认终端
 git clone https://github.com/chengpengzhao/My_vimrc.git 
 cd My_vimrc
 git checkout dev-WSL
-mv ./.vimrc ~/.vimrc
-mv ./.zshrc ~/.zshrc
-sudo apt-get install ripgrep
-# mv ./'.vimrc(ssh)' ../.vimrc
-#sudo apt install build-essential cmake python3-dev
-#cd ~/.vim/bundle/YouCompleteMe
-#python3 install.py --all
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #安装oh-my-zsh
+cp .vimrc ~/.vimrc
+cp .zshrc ~/.zshrc
+mkdir ~/.vim/UltiSnips
+cp *.snippets ~/.vim/UltiSnips/
+# cp ./'.vimrc(ssh)' ../.vimrc
+cd
 source ~/.zshrc
+# 这一步完成后进入vim进行插件配置
+# ==========================================================================
+# 第三步，youcompleteme安装（安装贼jb复杂的插件代表）以及github配置
+cd ~/.vim/plugged/YouCompleteMe
+python3 install.py --all
+
+git config --global user.name "chengpengzhao"
+git config --global user.email "chengpengzhao@foxmail.com"
+git config --global core.quotepath false  #防止用tab键自动补全的中文文件名乱码
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global core.safecrlf true
+ssh-keygen -t rsa -C "chengpengzhao@foxmail.com"
+
+vim ~/.ssh/id_rsa.pub     #  复制添加到github
+
+
 # sudo curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 # sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
