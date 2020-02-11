975f02a70f8e0e64b5c10d6142adf7f96005c1f0
chengpengzhao <chengpengzhao@foxmail.com>
2020-02-11 14:44:16 +0900

    update daily

--- initial_ssh.sh
+++ initial_ssh.sh
@@ -39,7 +39,7 @@ cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 mkdir ~/.vim/UltiSnips
 cp *.snippets ~/.vim/UltiSnips/
-# cp ./'.vimrc(ssh)' ../.vimrc
+# cp ./'.vimrc(ssh)' ~/.vimrc
 source ~/.zshrc
 # 这一步完成后进入vim进行插件配置
 # ==========================================================================
@@ -58,7 +58,37 @@ ssh-keygen -t rsa -C "chengpengzhao@foxmail.com"
 vim ~/.ssh/id_rsa.pub     #  复制添加到github
 
 
-# sudo curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
+# ==========================================================================
+
+sudo curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+systemctl start docker
+systemctl enable docker
+cd ~ && mkdir bitwarden && cd bitwarden
+
+cat >> docker-compose.yml << EOF
+version: '3'
+
+services:
+  bitwarden:
+    image: bitwardenrs/server:latest
+    container_name: bitwarden
+    restart: always
+    volumes:
+      - ./data:/data
+    env_file:
+      - config.env
+    ports:
+      - "8081:80"
+EOF
+
+cat >> config.env << EOF
+DOMAIN=https://bw.example.com
+DATABASE_URL=/data/bitwarden.db
+ROCKET_WORKERS=10
+WEB_VAULT_ENABLED=true
+SIGNUPS_ALLOWED=true
+EOF
+docker-compose up -d
+docker-compose down && docker-compose up -d
