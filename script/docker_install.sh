62a90a138fb8e5b68a2f14144964cfd8dc255972
chengpengzhao <cavsarpwgnckamekerokardank89@gmail.com>
2020-07-30 23:56:16 +0800

    加入docker-compose文件

--- script/docker_install.sh
+++ script/docker_install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
 sudo curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 wait
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo cp ../config/docker-compose-Linux-x86_64   /usr/local/bin/docker-compose
 wait
 sudo chmod +x /usr/local/bin/docker-compose
-systemctl start docker
-systemctl enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
